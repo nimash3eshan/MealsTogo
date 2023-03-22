f97e35bb02e964f21123826f8d26a3f567445cea
nimash eshan <csenanayake37@gmail.com>
2023-03-22 15:24:14 +0530

    bottom tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,71 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {RestaurantScreen} from './src/features/restaurants/screens/restaurants.screen';
 import {ThemeProvider} from 'styled-components/native';
 import {theme} from './src/infrastructure/theme';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function MapScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Map!</Text>
+    </View>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Restaurant') {
+            iconName = focused ? 'restaurant' : 'restaurant-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'Map') {
+            iconName = focused ? 'map' : 'map-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen
+        name="Restaurant"
+        component={RestaurantScreen}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <MyTabs />
+        </NavigationContainer>
       </ThemeProvider>
       <StatusBar style="auto" />
     </>
